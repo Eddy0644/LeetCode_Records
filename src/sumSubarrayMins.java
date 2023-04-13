dcea4e38113e695e0bb22ca7868d8e3f63e1140a
netcy <a@nc32.top>
2023-04-13 21:07:08 +0800

    .

--- src/sumSubarrayMins.java
+++ src/sumSubarrayMins.java
@@ -2,15 +2,21 @@ class Solution {
     public static int sumSubarrayMins(int[] arr) {
         int M = 1000000007;
         if (arr.length == 1) return arr[0];
-        int min_sum = 0;
+        int min_sum = 0, temp_min = -1;
         for (int i = 0, j = 0; i < arr.length; ) {
-            int temp_min = arr[i];
-            for (int k = i; k <= j; k++) temp_min = Math.min(arr[k], temp_min);
+            if (temp_min == -1) {
+                temp_min = arr[i];
+//                for (int k = i; k <= j; k++) temp_min = Math.min(arr[k], temp_min);
+            } else {
+                //only j changed since last iteration
+                temp_min = Math.min(arr[j], temp_min);
+            }
             if (min_sum + temp_min > M) min_sum -= M;
             min_sum += temp_min;
             if (j == arr.length - 1) {
                 i++;
                 j = i;
+                temp_min = -1;
             } else j++;
         }
         return min_sum % (1000000007);
@@ -19,8 +25,8 @@ class Solution {
 
 public class sumSubarrayMins {
     public static void main(String[] args) {
-//        System.out.println(Solution.sumSubarrayMins(new int[]{3, 1, 2, 4}));
-        System.out.println(Solution.sumSubarrayMins(new int[]{11,81,94,43,3}));
+        System.out.println(Solution.sumSubarrayMins(new int[]{3, 1, 2, 4}));
+        System.out.println(Solution.sumSubarrayMins(new int[]{11, 81, 94, 43, 3}));
         System.out.println("Run successful.");
     }
 }
