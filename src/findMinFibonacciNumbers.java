413ad612de8bb0dc9035ec59606dc7472b497473
netcy <a@nc32.top>
2023-04-21 19:29:17 +0800

    .rewrite

--- src/findMinFibonacciNumbers.java
+++ src/findMinFibonacciNumbers.java
@@ -1,22 +1,23 @@
+import java.util.ArrayList;
 import java.util.Vector;
 
 class Solution {
     public static int findMinFibonacciNumbers(int k) {
-        // first get nearest fibon number
-//        int[] fibon=new int[];
-        Vector<Integer> fibonacci = new Vector<>();
-        fibonacci.addElement(1);
+        // rewritten with arraylist, got better records
+        // first get nearest fibonacci number
+        ArrayList<Integer> fibonacci = new ArrayList<>();
+        fibonacci.add(1);
         int lastVal = 2, lastIndex = 1;
         while (k >= lastVal) {
             // At here to addElement to avoid lastVal > k
-            fibonacci.addElement(lastVal);
-            lastVal = fibonacci.elementAt(lastIndex) + fibonacci.elementAt(lastIndex - 1);
+            fibonacci.add(lastVal);
+            lastVal = fibonacci.get(lastIndex) + fibonacci.get(lastIndex - 1);
             lastIndex++;
         }
         int nowIndex = lastIndex - 1, nowVal, count = 0;
         while (k > 0) {
             if (nowIndex < 0) return -1;
-            nowVal = fibonacci.elementAt(nowIndex);
+            nowVal = fibonacci.get(nowIndex);
             if (k >= nowVal) {
                 k -= nowVal;
                 count++;
@@ -32,7 +33,7 @@ class Solution {
 public class findMinFibonacciNumbers {
     public static void main(String[] args) {
         System.out.println(Solution.findMinFibonacciNumbers(5));
-//        System.out.println(Solution.findMinFibonacciNumbers(19));
+        System.out.println(Solution.findMinFibonacciNumbers(19));
         System.out.print("Run successful.");
     }
 }
