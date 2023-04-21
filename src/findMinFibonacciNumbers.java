9484797bc027ed91325f9c59161cf251ce609a2b
netcy <a@nc32.top>
2023-04-21 19:18:31 +0800

    .

--- src/findMinFibonacciNumbers.java
+++ src/findMinFibonacciNumbers.java
@@ -4,27 +4,28 @@ class Solution {
     public static int findMinFibonacciNumbers(int k) {
         // first get nearest fibon number
 //        int[] fibon=new int[];
-        Vector<Integer> fibonacci= new Vector<>();
+        Vector<Integer> fibonacci = new Vector<>();
         fibonacci.addElement(1);
-        int lastVal=2,lastIndex=1;
-        while(k>lastVal){
+        int lastVal = 2, lastIndex = 1;
+        while (k > lastVal) {
             // At here to addElement to avoid lastVal > k
             fibonacci.addElement(lastVal);
-            lastVal=fibonacci.elementAt(lastIndex)+fibonacci.elementAt(lastIndex-1);
+            lastVal = fibonacci.elementAt(lastIndex) + fibonacci.elementAt(lastIndex - 1);
             lastIndex++;
         }
-        int nowIndex=lastIndex,nowVal,count=0;
-        while(k>0){
-            if(nowIndex<0)return -1;
-            nowVal=fibonacci.elementAt(nowIndex);
-            if(k>=nowVal){
-                k-=nowVal;count++;
-            }else{
+        int nowIndex = lastIndex, nowVal, count = 0;
+        while (k > 0) {
+            if (nowIndex < 0) return -1;
+            nowVal = fibonacci.elementAt(nowIndex);
+            if (k >= nowVal) {
+                k -= nowVal;
+                count++;
+            } else {
                 nowIndex--;
             }
         }
-        if(k==0)return count;
-            else return -1;
+        if (k == 0) return count;
+        else return -1;
     }
 }
 
