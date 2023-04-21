4804eedf4bcae575c879bbdd491c9d9747cb0a62
netcy <a@nc32.top>
2023-04-21 21:14:18 +0800

    .success

--- src/getHappyString.java
+++ src/getHappyString.java
@@ -2,30 +2,35 @@ import java.util.Vector;
 
 class Solution {
     public static String getHappyString(int n, int k) {
+        if (n == 1) {
+            return (k == 3) ? "c" : (k == 2 ? "b" : (k == 1 ? "a" : ""));
+        }
         int layer = n, layerSize = 2;
         // layer means the l-th level
         int[] layers = new int[n + 1];
         layers[n] = 1;
-        while (layer >= 0 && k > layerSize) {
+        while (layer > 0 && k > layerSize) {
             //size of layer n-1
             layers[layer - 1] = layerSize;
             layer--;
             layerSize *= (layer == 1) ? 3 : 2;
         }
+        if (layer == 0) return "";
         layers[layer - 1] = layerSize;
-        if (k > layerSize) return "";
         int[] sequence = new int[n - layer + 1];
         int initial = layer;
 //        int[] result = getNthSequence(layer + 1, layerSize / ((layer == 0) ? 3 : 2), n, k, original);
 //        layer+=1;
         if (layer == 1) {
             // solve layer-1 issue
-            if (layers[0] > k && k > 2 * layers[1]) {
+            if (layers[0] >= k && k > 2 * layers[1]) {
                 sequence[0] = 3;
                 k -= 2 * layers[1];
-            } else if (2 * layers[1] > k && k > layers[1]) {
+            } else if (2 * layers[1] >= k && k > layers[1]) {
                 sequence[0] = 2;
                 k -= layers[1];
+            } else {
+                sequence[0] = 1;
             }
             layer++;
         }
@@ -42,10 +47,11 @@ class Solution {
         }
         // translate sequence into abc
         StringBuffer ans = new StringBuffer();
-        char lastChr ='c';
-        if(n> sequence.length){
+        char lastChr = 'c';
+        boolean skip1stFlag = false;
+        if (n > sequence.length) {
             // non-sense default value
-            for (int i = 0; i < n-sequence.length; i++) {
+            for (int i = 0; i < n - sequence.length; i++) {
                 Vector<Character> chr = new Vector<>();
                 chr.addElement('a');
                 chr.addElement('b');
@@ -54,16 +60,17 @@ class Solution {
                 lastChr = chr.elementAt(0);
                 ans.append(lastChr);
             }
-        }else
+        } else {
             lastChr = (sequence[0] == 3) ? 'c' : (sequence[0] == 2 ? 'b' : 'a');
-        boolean flag = true;
-//        char
-//        ans.append(lastChr);
+            ans.append(lastChr);
+            skip1stFlag = true;
+        }
+
         for (int each : sequence) {
-//            if (flag) {
-//                flag = false;
-//                continue;
-//            }
+            if (skip1stFlag) {
+                skip1stFlag = false;
+                continue;
+            }
             Vector<Character> chr = new Vector<>();
             chr.addElement('a');
             chr.addElement('b');
@@ -72,24 +79,17 @@ class Solution {
             lastChr = each == 2 ? chr.elementAt(1) : chr.elementAt(0);
             ans.append(lastChr);
         }
-        return "";
+        return ans.toString();
     }
 
-//    public static int[] getNthSequence(int layer, int layerSize, int n, int k, int[] upper) {
-//        if (layer > n) return upper;
-//        int develSize = layerSize / 2;
-//        if (layerSize > k && k > develSize) {
-//            // at this layer
-//        } else if (develSize > k) {
-//        }
-//        return new int[1];
-//    }
 }
 
 public class getHappyString {
     public static void main(String[] args) {
+        System.out.println(Solution.getHappyString(3, 12));
+//        System.out.println(Solution.getHappyString(2, 4));
 //        System.out.println(Solution.getHappyString(3, 9));
-        System.out.println(Solution.getHappyString(10, 100));
+//        System.out.println(Solution.getHappyString(10, 100));
         // 2 2 1 1 1 2 1
         System.out.print("Run successful.");
     }
