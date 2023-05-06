6ccc1508ff9fbc3a575dcc2813c4b6ae112e3b89
netcy <a@nc32.top>
2023-05-06 21:17:50 +0800

    .

--- src/flatten.java
+++ src/flatten.java
@@ -1,2 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution14 {
+    public static void flatten(TreeNode root) {
+        //https://leetcode.cn/problems/flatten-binary-tree-to-linked-list/
+        //https://leetcode.cn/problems/flatten-binary-tree-to-linked-list/
+        //https://leetcode.cn/problems/flatten-binary-tree-to-linked-list/
+    }
+}
+
 public class flatten {
+    public static void main(String[] args) {
+//        System.out.println(Solution.flatten());
+        System.out.print("Run successful.");
+    }
 }
